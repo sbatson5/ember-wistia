6d165f937422bc886cf8036f844df2d22bcdc9d7
Scott Batson <sjbats@email.com>
2016-10-20 12:43:31 -0400

    add integration test for passed action

--- tests/integration/components/wistia-video-test.js
+++ tests/integration/components/wistia-video-test.js
@@ -1,8 +1,28 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import Ember from 'ember';
+
+const {
+  K,
+  Service,
+  RSVP: { Promise }
+} = Ember;
 
 moduleForComponent('wistia-video', 'Integration | Component | wistia video', {
-  integration: true
+  integration: true,
+  beforeEach() {
+    const stubbedWistia = Service.extend({
+      addVideo: K,
+      getCurrentlyPlaying: K,
+      getVideo: function() {
+        return new Promise((resolve) => {
+          resolve({});
+        });
+      }
+    });
+    this.register('service:wistia', stubbedWistia);
+    this.inject.service('wistia', { as: 'wistia' });
+  }
 });
 
 test('it has a css class prefixed with wistia_async', function(assert) {
@@ -12,4 +32,14 @@ test('it has a css class prefixed with wistia_async', function(assert) {
 
   const videoDiv = this.$().find('.wistia_embed:eq(0)');
   assert.ok(videoDiv.hasClass('wistia_async_scottIsAwesome'), 'async class is added');
+});
+
+test('`videoInitialize` method is fired once component renders', function(assert) {
+  assert.expect(1);
+
+  this.set('videoInitialize', () => {
+    assert.ok(true, 'initialize function is called');
+  });
+
+  this.render(hbs`{{wistia-video matcher="scottIsAwesome" videoInitialize=videoInitialize}}`);
 });
